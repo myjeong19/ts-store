a33e852b2ce372e3c5e46cea234340cdf8c129ab
myjeong19 <myjeong19@naver.com>
2025-02-11 18:29:02 +0900

    💄 Add cart button to navbar

--- src/widgets/navbar/ui/cart-button.tsx
+++ src/widgets/navbar/ui/cart-button.tsx
@@ -1,1 +1,20 @@
-export const CartButton = () => <div>CartButton</div>;
+import { LinkButton } from '@/shared/ui';
+import { ShoppingCart } from 'lucide-react';
+
+export const CartButton = () => {
+  const NUM_ITEMS_IN_CART = 5;
+
+  return (
+    <LinkButton
+      to="/cart"
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <ShoppingCart />
+      <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+        {NUM_ITEMS_IN_CART}
+      </span>
+    </LinkButton>
+  );
+};
