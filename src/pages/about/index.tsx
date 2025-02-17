789aebae09bccba693ea62c4349d9f63e2d78b7b
myjeong19 <myjeong19@naver.com>
2025-02-17 09:31:57 +0900

    💄 About page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,19 @@
-const About = () => <>About</>;
+const About = () => (
+  <section>
+    <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text:6xl">
+      We Love
+      <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">comfy</span>
+    </h1>
+    <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+      galley of type and scrambled it to make a type specimen book. It has survived not only five
+      centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
+      was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+      passages, and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
+    </p>
+  </section>
+);
 
 export default About;
