61269b76648fd847953040de170533f2dbce58ed
myjeong19 <myjeong19@naver.com>
2025-02-19 13:25:14 +0900

    ♻️ Simplify landing page component

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,18 +1,11 @@
-import { ProductsResponse } from '@/entities/product/types';
 import { Products } from '@/features/products/ui';
 import { Hero } from '@/widgets/landing/ui';
-import { useLoaderData } from 'react-router';
 
-const Landing = () => {
-  const result = useLoaderData() as ProductsResponse;
-  console.log(result);
-
-  return (
-    <>
-      <Hero />
-      <Products />
-    </>
-  );
-};
+const Landing = () => (
+  <>
+    <Hero />
+    <Products />
+  </>
+);
 
 export default Landing;
--- src/widgets/landing/ui/hero.tsx
+++ src/widgets/landing/ui/hero.tsx
@@ -1,22 +1,20 @@
 import { ButtonLink } from '@/shared/ui';
 import { HeroCarousel } from './hero-carousel';
 
-export const Hero = () => {
-  return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 items-center gap-24">
-      <div>
-        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
-          We are changing the way people shop
-        </h1>
-        <p className="mt-8 max-w-xl text-lg leading-8">
-          "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-          velit..."
-        </p>
-        <ButtonLink variant="default" size="lg" className="mt-10" to="/products">
-          Our Products
-        </ButtonLink>
-      </div>
-      <HeroCarousel />
-    </section>
-  );
-};
+export const Hero = () => (
+  <section className="grid grid-cols-1 lg:grid-cols-2 items-center gap-24">
+    <div>
+      <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+        We are changing the way people shop
+      </h1>
+      <p className="mt-8 max-w-xl text-lg leading-8">
+        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+        velit..."
+      </p>
+      <ButtonLink variant="default" size="lg" className="mt-10" to="/products">
+        Our Products
+      </ButtonLink>
+    </div>
+    <HeroCarousel />
+  </section>
+);
