9a26a26cd087dfc7d4e58a5c98424824b13d7688
myjeong19 <myjeong19@naver.com>
2025-02-21 06:09:34 +0900

    🐛 Set fixed width in Skeleton component to prevent CLS

--- src/shared/shadcn-ui/skeleton.tsx
+++ src/shared/shadcn-ui/skeleton.tsx
@@ -1,15 +1,12 @@
-import { cn } from "@/shared/lib/utils"
+import { cn } from '@/shared/lib/utils';
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn('animate-pulse rounded-md bg-primary/10 width-[calc(100vw-8px)]', className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
